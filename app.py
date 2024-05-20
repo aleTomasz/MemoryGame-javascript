70a186b866f28ff81cb1bf76da717e6b1981cdd8
miloszfede <miloszfede@gmail.com>
2024-05-20 17:07:12 +0200

    kontynuacja ogarniania rejestracji, javascript dalej nie jest czytany przez flaska :(

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, render_template, session
+import jinja2
+import database_common
 
 app = Flask(__name__)
 app.secret_key = "b6128f50-6f58-46ba-adca-a50ea3c9b9d2"
@@ -7,12 +8,22 @@ app.secret_key = "b6128f50-6f58-46ba-adca-a50ea3c9b9d2"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
+    else:
+        return render_template('index.html')
 
 @app.route('/registration')
 def registration():
     return render_template('registration_page.html')
 
 @app.route('/login')
-def login_page():
-    return render_template('login_page.html')
+def login(cursor):
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        cursor.execute("""SELECT login, password FROM users WHERE login = '{login}' and password = '{password}'""")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
